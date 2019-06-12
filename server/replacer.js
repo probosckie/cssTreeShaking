6f845a04ea14a73542fc06c599d7d7778cef02f0
Prabhas Joshi <joshi.prabhas@gmail.com>
2019-06-12 14:39:05 +0530

    Update replacer.js

--- server/replacer.js
+++ server/replacer.js
@@ -8,16 +8,14 @@ import { getContent, writeToFile, extractAllClasses, getAllCssRules, isClassPart
 
 
 let jsFile = '../files/src/components/Bus/BusReviews/index.js';
-let cssFile = '../files/sassStyles.css';
+let cssFile = '../files/src/components/Bus/BusReviews/styles.css';
 
-let pcTree;
-
-let writeCssFile = '../postcssMapping.json';
-writeCssFile = path.join(__dirname, writeCssFile);
-
+let cssRuleFile = '../cssResult.json';
 
 jsFile = path.join(__dirname, jsFile);
 cssFile = path.join(__dirname, cssFile);
+
+let pcTree;
 
 function isClassNamePresentInSelectorString(className, selectorString) {
 	className = '.' + className;
@@ -42,12 +40,6 @@ function isClassNamePresentInSelectorString(className, selectorString) {
 
 	return result;
 }
-
-//let c1 = '.filterHeading', c2 = '.filterHeading';
-//console.log(` Is class ${c1} part of the complex selector rule ${c2}`);
-//console.log(isClassnamePartOfComplexSelectorRule(c1, c2));
-
-
 
 function isClassnamePartOfComplexSelectorRule(className, selector) {
 	let charlastTest, test, match, charStart;
@@ -84,19 +76,51 @@ function isClassnamePartOfComplexSelectorRule(className, selector) {
 	return test;
 }
 
+function isMediaQueryBlock(seed){
+	return seed.type === 'atrule' && seed.name === 'media'
+}
+
+function isSassNode(seed) {
+	let nodes = seed.nodes, test = false;
+	if(nodes && nodes.length){
+		nodes.forEach(v => {
+			if(v.type === 'rule')
+				test = true;
+		});
+	}
+	return test;
+}
+
+
+function returnStyleFromNode(node){
+	let style = {};
+	if(node.nodes && node.nodes.length){
+		node.nodes.forEach(v => {
+			if(v.type === 'decl')
+				style[v.prop] = v.value;
+		});
+	}
+	return style;
+}
 
 function recurseFindComplex(c, tree){
-	let isComplex = false, isFound = false, isMedia = false;
+	let isComplex = false, isFound = false, isMedia = false, hasInternalNodes = false;
 	let leafLevel = -1;
 
-	function recurseFind(selectorName, seed, isM, level){
-		let isAtRule = seed.type === 'atrule' && seed.name === 'media', result;
+	function recurseFind(selectorName, seed, isM, level) {
+		let isAtRule = isMediaQueryBlock(seed), result;
 		if(seed.selector){
 			result = isClassNamePresentInSelectorString(selectorName, seed.selector);
+
 			if(result.complexSelectorTest)
 				isComplex = true;
+			
 			if(result){
 				isFound = true;
+
+				if(!result.complexSelectorTest && isSassNode(seed))
+					hasInternalNodes = true;
+
 				if(isM)
 					isMedia = true;
 				if(level > leafLevel)
@@ -114,52 +138,48 @@ function recurseFindComplex(c, tree){
 	if(isComplex || leafLevel > 1 || isMedia || !isFound)
 		return false;
 
+	if(hasInternalNodes)
+		return 'classNameReplace';
+
 	return true;
 }
 
-
 async function start(){
 	let jsContent = await getContent(jsFile);
-	//console.log(jsContent);
-	let lines = jsContent.split('\n');
+	let test, lines = jsContent.split('\n');
 	let classes = extractAllClasses(lines, true, 's');
-	/*if(classes)
+	if(classes)
 		classes = Object.keys(classes);
-	console.log(classes);*/
 	let cssContent = await getContent(cssFile);
 	let rule = pc.parse(cssContent);
-	/*let modified = rule.nodes.map(v => {
-		if(v.raws)
-			delete v.raws;
-		if(v.source)
-			delete v.source;
-		if(v.nodes) {
-			v.nodes = v.nodes.map(v1 => {
-				if(v1.raws)
-					delete v1.raws;
-				if(v1.source)
-					delete v1.source;
-				return v1;
+	pcTree = rule;
+	let modifiedClassList = [], classNameReplaceClassList = [];
+	for(let i of classes) {
+		test = recurseFindComplex(i, pcTree);
+		if(test === 'classNameReplace')
+			classNameReplaceClassList.push(i);
+		else if (test === true)
+			modifiedClassList.push(i);
+	}
+	let parsedStylesForClasses = [...classNameReplaceClassList, ...modifiedClassList].reduce((a,v) => {
+		a['.' + v] = {};
+		return a;
+	},{});
+	let styleNodeList = pcTree.nodes.filter(v => !isMediaQueryBlock(v) && v.type !== 'comment');
+	for(let i of styleNodeList){
+		if(i.selector){
+			let present = false, x = i.selector.split(',');
+			x.forEach(v => {
+				if(v in parsedStylesForClasses){
+					parsedStylesForClasses[v] = Object.assign({}, parsedStylesForClasses[v], returnStyleFromNode(i));
+				}
 			});
 		}
-		return v;
-	});*/
+	}
 
-	pcTree = rule;
-	test();
-	//await writeToFile(writeCssFile, JSON.stringify(modified));
-	//console.log('file written');
+	
+	//console.log(parsedStylesForClasses);
 }
 
 
-function test(){
-	//let cssContent = await getContent(writeCssFile);
-	//let rule = pc.parse(cssContent);
-	console.log('Is class splittable ??');
-	console.log(recurseFindComplex('filterHeading', pcTree));
-}
-
-
-//test();
 start();
-
