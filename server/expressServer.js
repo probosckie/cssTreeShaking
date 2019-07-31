7eaa111511a21131b7c235db859e49a18bf467a2
Prabhas Joshi <joshi.prabhas@gmail.com>
2019-07-31 15:47:41 +0530

    Update expressServer.js

--- server/expressServer.js
+++ server/expressServer.js
@@ -3,18 +3,28 @@ import socket from 'socket.io';
 import path from 'path';
 import fs from 'fs';
 
-import { createFullFileName, getContent, findStartEndClassnameParenthesis, processJs, processCss, findLineNumbersForClassInCss } from './utils';
+import { createFullFileName, getContent, findStartEndClassnameParenthesis, processJs, processCss, findLineNumbersForClassInCss, findLineNumbersForClassInJs } from './utils';
 import { isThereACssAsDependencyAt1stLevel, buildDependencyTree, returnAllDependencies } from './traverse';
+import { processCssBreakdown, cssPruning } from './replacer';
+import { findClassLineNumbersInCss } from './sassAst';
 
 const app = express();
 
-const server = app.listen(3000);
+const server = app.listen(3051);
 
 app.use(express.static(path.join(__dirname, '../client/public')));
 
 const io = socket(server);
 
+let newBase = '/Users/Prabhas/code/consumer-web-bus/src';
+
 io.sockets.on('connection', newConnection);
+
+function calcFileSize(x){
+	let s = fs.statSync(x);
+	if(s)
+		return s.size;
+}
 
 function newConnection(socket) {
 	console.log(`connected to socket ${socket.id}`);
@@ -22,27 +32,66 @@ function newConnection(socket) {
 	socket.on('highlightFile', highlightSource);
 	socket.on('fixIssues', fixIssues);
 	socket.on('saveCssFile', saveCssFile);
-	socket.on('fetchAllDependentFiles', getAllDependentFiles)
+	socket.on('saveJsFile', saveJsFile);
+	socket.on('fetchAllDependentFiles', getAllDependentFiles);
+	socket.on('passD', passD);
+	socket.on('fetchBreakableClassList', createCssBreakdown);
+	socket.on('findClassLineNumbers', findClassInSrc)
 
+
+	async function createCssBreakdown(data){
+		let trybreakdown = processCssBreakdown(data.jsfilePath, data.cssFilePath, data.jsPaneContent, data.cssPaneContent);
+
+		trybreakdown.then(v => {
+			io.sockets.emit('cssBreakdown', v);
+		});
+	}
 
 	async function getFileSource( {filePath} ){
 		let tree = buildDependencyTree(filePath);
 		let cssDependency;
+		let jsSize = '', cssSize = '';
 		let breakup = isThereACssAsDependencyAt1stLevel(tree);
-
 		if ('css' in breakup) {
 			cssDependency = breakup.css;
 		}
 		let fullName = createFullFileName(filePath);
+		jsSize = calcFileSize(fullName);
 		let content = await getContent(fullName);
+		
+
 		io.sockets.emit('fileSourceJs', content);
-		io.sockets.emit('setFullPathJs', fullName); 
+		io.sockets.emit('setFullPathJs', fullName);
+		io.sockets.emit('fileSizeJs', {
+			filePath: fullName,
+			originalSize: jsSize
+		});
+
+		cssSize = calcFileSize(cssDependency);
+		
+
+
 		if(cssDependency){
 			content = await getContent(cssDependency);
 			io.sockets.emit('fileSourceCss', content);
 			io.sockets.emit('setFullPathCss', cssDependency);
+			io.sockets.emit('fileSizeCss', {
+				filePath: cssDependency,
+				originalSize: cssSize
+			});
 		}
 	}
+
+
+	async function passD( data ){
+		console.log('got data', data);
+		//io.sockets.emit('fileSourceCss', content);
+		io.sockets.emit('sendRData', 'response from data');
+		
+	}
+
+
+
 
 	async function highlightSource( {filePath} ){
 		let fullName = createFullFileName(filePath);
@@ -55,61 +104,48 @@ function newConnection(socket) {
 
 	async function fixIssues({ js, css }){
 		if(js && css){
-			let cssDataSet = await processCss(css);
-			let jsDataSet = await processJs(js);
-
-			let contentJs = await getContent(js);
-			let contentCss = await getContent(css);
-			
-			//find styles which are not used in jsx
-			let unusedClasses = [], unusedClassesLineNumbers = [];
-			for(let i in cssDataSet){
-				if(!(i.slice(1) in jsDataSet))
-					unusedClasses.push(i.slice(1));
-			}
-
-			//printing line numbers of unused classes
-			unusedClasses.forEach(v => {
-				unusedClassesLineNumbers = unusedClassesLineNumbers.concat(findLineNumbersForClassInCss(v, contentCss));
-			});
-
-			
-
-			//find classnames for which styles are not defined in css
-			/*let notDefinedClasses = [];
-			for(let i in jsDataSet){
-				if(!(`.${i}` in cssDataSet))
-					notDefinedClasses.push(i);
-			}*/
-
-			//Find empty styles - which have empty body
-			let emptyClasses = [], emptyClassesLineNumbers = [];
-			for(let i in cssDataSet){
-				if(cssDataSet[i] && Object.keys(cssDataSet[i]).length === 0)
-					emptyClasses.push(i.slice(1));
-			}
-
-			emptyClasses.forEach(v => {
-				emptyClassesLineNumbers = emptyClassesLineNumbers.concat(findLineNumbersForClassInCss(v, contentCss));
-			});
-
-			
-			if(unusedClassesLineNumbers.length){
-				io.sockets.emit('unusedClassesCss', unusedClassesLineNumbers);
-			}
-
-
-			if(emptyClassesLineNumbers.length){
-				io.sockets.emit('emptyClassesCss', emptyClassesLineNumbers);
-			}
+			let pruning = cssPruning(js, css);
+			io.sockets.emit('cssRedundancies', pruning);
 		}
 	}
 
 
 	async function saveCssFile(obj){
-		fs.writeFile(obj.name, obj.value, (err) => {
-			console.log('file is saved - check bete');
-			io.sockets.emit('cssFileSaved', 'success');
+		let parts = obj.name.split('src');
+		let newPath = newBase + parts[1];
+		fs.writeFile(newPath, obj.value, (err) => {
+			let cssSize = calcFileSize(newPath);
+			
+			io.sockets.emit('fileSizeCssNew',{
+				filePath: obj.name,
+				newSize: cssSize
+			});
+			setTimeout(() => {
+				io.sockets.emit('cssFileSaved', {
+					fileName: obj.name,
+					status: 'success'
+				});	
+			}, 400);
+		})
+	}
+
+	async function saveJsFile(obj){
+		let parts = obj.name.split('src');
+		let newPath = newBase + parts[1];
+
+		fs.writeFile(newPath, obj.value, (err) => {
+			let jsSize = calcFileSize(newPath);
+			io.sockets.emit('fileSizeJsNew',{
+				filePath: obj.name,
+				newSize: jsSize
+			});
+			setTimeout(() => {
+				io.sockets.emit('jsFileSaved', {
+					fileName: obj.name,
+					status: 'success'
+				});
+			}, 400);
+			
 		})
 	}
 
@@ -117,5 +153,13 @@ function newConnection(socket) {
 	async function getAllDependentFiles(fileName){
 		let dep = await returnAllDependencies(fileName);
 		io.sockets.emit('allDependencies', dep);
+	}
+
+	async function findClassInSrc(d){
+		let jsX = d.jsPaneContent, cssC = d.cssPaneContent, className = d.className;
+		let linesInJSX = findLineNumbersForClassInJs(className, jsX);
+		let linesInCss = await findClassLineNumbersInCss(cssC, className);
+		//console.log(linesInCss);
+		io.sockets.emit('classnameLines', { linesInJSX, linesInCss, className });
 	}
 }
